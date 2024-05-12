f1a66e80cf3a66c1f3e0a96f751ee3ac2010fe95
Bruno <bruno.cvp@hotmail.com>
2024-05-12 02:25:52 -0300

    Aprendizado Basico de JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+// FUNÇÕES
+
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaTeclas = document.querySelectorAll('.tecla')
+
+listaTeclas[0].onclick = tocaSomPom;
