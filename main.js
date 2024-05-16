b94cac6020843e48ad7e5c7cce7cd7f749b28e83
Bruno <bruno.cvp@hotmail.com>
2024-05-15 22:02:20 -0300

    Update Teclas e Segurança

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const listaTeclas = document.querySelectorAll('.tecla');
 
 function tocaSom(idAudio) {
-    document.querySelector(idAudio).play();
+    const elemento = document.querySelector(idAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
 }
 
 for(let contador = 0; contador < listaTeclas.length; contador++) {
@@ -11,6 +17,20 @@ for(let contador = 0; contador < listaTeclas.length; contador++) {
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = function() {
         tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento)
+
+        if (evento.code == 'Enter' || evento.code == 'Space') {
+            tecla.classList.add('ativa')
+        }
+    }
+    tecla.onkeyup = function(evento) {
+        if (evento.code == 'Enter' || evento.code == 'Space') {
+            tecla.classList.remove('ativa')
+        }
     }
 
 }
